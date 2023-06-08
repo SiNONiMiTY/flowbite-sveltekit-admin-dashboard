f9c19a1a9d1bf53bbc7ee78daf88672d4d820043
Lindjunne Montenegro II <lgbmontenegroii@outlook.ph>
2023-06-08 09:12:13 +0800

    chore_sinomity: 20230608-v2 Update - Bump package dependency versions - Add new pages
    Signed-off-by: Lindjunne Montenegro II <lgbmontenegroii@outlook.ph>

--- src/lib/layouts/Sidebar.svelte
+++ src/lib/layouts/Sidebar.svelte
@@ -173,22 +173,28 @@
                         <SidebarDropdownItem
                             label="Sign in"
                             aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
-                            href="/login"
+                            href="/authentication/login"
                             />
                         <SidebarDropdownItem
                             label="Sign up"
-                            aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"/>
+                            aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
+                            href="/authentication/sign-up"
+                            />
                         <SidebarDropdownItem
                             label="Forgot password"
                             aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
-                            href="/forgot-password"
+                            href="/authentication/forgot-password"
                             />
                         <SidebarDropdownItem
                             label="Reset password"
-                            aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"/>
+                            aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
+                            href="/authentication/reset-password"
+                            />
                         <SidebarDropdownItem
                             label="Profile lock"
-                            aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"/>
+                            aClass="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100  pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
+                            href="/authentication/profile-lock"
+                            />
                     </SidebarDropdownWrapper>
                     <SidebarDropdownWrapper
                         btnClass="flex items-center w-full p-2 text-base text-gray-900 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
